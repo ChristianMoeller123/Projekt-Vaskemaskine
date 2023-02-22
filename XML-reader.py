b885ad20121c06ada1653d62eead76af4b58abfb
chris <christian.barssa@gmail.com>
2023-02-22 16:31:53 +0100

    Test commit

--- XML-reader.py
+++ XML-reader.py
@@ -9,7 +9,7 @@ Action and Child. All the data from the XML file is kept in the instances.
 """
 # Importing libraries
 import xml.etree.ElementTree as ET #XML
-import re #Digits in strings
+import re # Library for handling strings
 import pickle # object read/write
 
 from PAC_Classes import Parent, Action, Child, Disassembly # Import PAC Classes
@@ -37,8 +37,8 @@ for i in range(len(rows)): #iterate over all rows
         cells[i].append(child.text) #Put all row data in a row in the list
 
 # Only keep the data values in the relevant cells, the rest is put in desc list
-desc = cells[0:4]
-del cells[0:4]
+desc = cells[0:5]
+del cells[0:5]
 
 # Creating empty lists for P/A/C instances:   
 Parents = []
@@ -49,15 +49,19 @@ Disassemblies = []
 # Sort the data and insert instances with the data
 for row in range(len(cells)):
     # Sorts the different IDs for Parents, actions, children and disassemblies
-    firstCellName = list(cells[row][0])
-    if firstCellName[1] == 'P': # A parent
+    IDAsList = list(cells[row][0])
+    IDAsLetters = " ".join(re.split("[^a-zA-Z]*", cells[row][0])).strip() #Keeps the letters, strip removes spaces
+    IDAsNumbers = " ".join(re.split("[^0-9-]*", cells[row][0])).strip() #Keeps the numbers and "-", strip removes spaces
+    IDForCurrent = cells[row][0][:cells[row][0].index('-')] #Finds the index of - and returns the strin UP TO "-"
+    IDForParent = cells[row][0][cells[row][0].index('-')+1:] #Finds the index of - and returns string FROM "-"
+    if IDAsLetters[0] == 'P': # A parent
         instance = Parent(cells[row][0], cells[row][1])
         Parents.append(instance)
         if len(cells[row]) == 7:
             disassemblyInstance(cells[row][2], cells[row][3], cells[row][4],\
                                 cells[row][5], cells[row][6], cells[row][0])
         print('fandt en Parent!')
-    elif firstCellName[1] == 'A': # An action
+    elif IDAsLetters[0] == 'A': # An action
         instance = Action(cells[row][0], cells[row][1], cells[row][2], cells[row][3],\
         cells[row][4], cells[row][5])
         Actions.append(instance)
@@ -65,7 +69,7 @@ for row in range(len(cells)):
             disassemblyInstance(cells[row][6], cells[row][7], cells[row][8],\
                                 cells[row][9], cells[row][10], cells[row][0])
         print('fandt en Action!')
-    elif firstCellName[1] == 'C' or firstCellName[1] == 'c': # A child
+    elif IDAsLetters[0] == 'C' or IDAsLetters[1] == 'c': # A child
         instance = Child(cells[row][0], cells[row][1], cells[row][2], cells[row][3])
         Children.append(instance)
         if len(cells[row]) == 9:
